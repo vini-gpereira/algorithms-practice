729aa848c7c2993659ff485a1b532427c0b1764d
viniciusgpereira <vini.gpereira2014@gmail.com>
2021-04-25 12:57:12 -0300

    feat(js): remove k from list

---
+++ javascript/remove_k_from_list.js
@@ -0,0 +1,15 @@
+// Singly-linked lists are already defined with this interface:
+// function ListNode(x) {
+//   this.value = x;
+//   this.next = null;
+// }
+//
+function removeKFromList(l, k) {
+    if (l) {
+        if (l.value === k) {
+            return 
+        }
+    }
+
+    return l;
+}
